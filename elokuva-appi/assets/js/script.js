b6481fee9a749b1e819b0c558b4dd53607f4c132
leokoivunen <99177879+leokoivunen@users.noreply.github.com>
2022-02-08 14:55:09 +0200

    Lisätty genre kohta

--- elokuva-appi/assets/js/script.js
+++ elokuva-appi/assets/js/script.js
@@ -5,6 +5,87 @@ const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const SEARCH_URL = BASE_URL + "/search/movie?" + API_KEY;
 
+const genre = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+
+console.log(genre);
+
 // Import HTML Elements
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -18,7 +99,6 @@ function getMovies(url) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.results);
       showMovies(data.results);
     });
 }
@@ -65,8 +145,8 @@ form.addEventListener("submit", (e) => {
   const searchTerm = search.value;
 
   if (searchTerm) {
-    getMovies(SEARCH_URL + '&query=' + searchTerm);
+    getMovies(SEARCH_URL + "&query=" + searchTerm);
   } else {
-      getMovies(API_URL);
+    getMovies(API_URL);
   }
 });
